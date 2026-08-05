c8a7ea19d6cab6cfa3820d3dd97bb36c84ce2434
VickyMVGL <mariaviclg2005@gmail.com>
2026-08-05 05:51:36 -0400

    Actualizando la landing para despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './proyecto-experiencia-personal/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
